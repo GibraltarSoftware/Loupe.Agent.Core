73aa1da4376ee742b6ee5fd8fa2e42df8da285c9
Kendall Miller <kendall@gibraltarsoftware.com>
2022-05-15 01:04:27 -0400

    Correct compilation error

--- src/test/MvcTestApp5/Startup.cs
+++ src/test/MvcTestApp5/Startup.cs
@@ -52,7 +52,7 @@ namespace MvcTestApp
             services.AddRazorPages()
                  .AddMicrosoftIdentityUI();
 
-            services.AddLoupe().AddEntityFrameworkCoreDiagnostics();
+            services.AddLoupe( builder => builder.AddEntityFrameworkCoreDiagnostics());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
